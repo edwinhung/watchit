77b8573cdb6493fc3bc8b2dd7b6bac6361ac593c
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-08 16:53:00 -0700

    run another js file with node

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const chokidar = require("chokidar");
 const debounce = require("lodash.debounce");
 const program = require("caporal");
 const fs = require("fs");
+const { spawn } = require("child_process");
 
 program
   .version("0.0.1")
@@ -17,7 +18,7 @@ program
     }
 
     const start = debounce(() => {
-      console.log("Start user program");
+      spawn("node", [name], { stdio: "inherit" });
     }, 100);
 
     chokidar
