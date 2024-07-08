607e1f72acf97cc38fa51a6420ec4252788993bb
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-07 19:40:00 -0700

    setup caporal to command line app

--- index.js
+++ index.js
@@ -2,13 +2,29 @@
 
 const chokidar = require("chokidar");
 const debounce = require("lodash.debounce");
+const program = require("caporal");
+const fs = require("fs");
 
-const start = debounce(() => {
-  console.log("Start user program");
-}, 100);
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
 
-chokidar
-  .watch(".")
-  .on("add", start)
-  .on("change", () => console.log("FILE CHANGED"))
-  .on("unlink", () => console.log("FILE UNLINKED"));
+    const start = debounce(() => {
+      console.log("Start user program");
+    }, 100);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
